cab31a9f53a662c9ac823c8e23b91aa32f3a7139
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2021-10-07 12:23:01 -0400

    add working Makefile for hello-world example

--- ch02/hello-world.c
+++ ch02/hello-world.c
@@ -1,6 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("Dual BSD/GPL")
+MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void) {
 	printk(KERN_ALERT "Hello, world\n");
